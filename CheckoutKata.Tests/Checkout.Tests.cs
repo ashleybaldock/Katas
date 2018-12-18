f05d938a2119a397206e99197800a504de42a370
Ashley Baldock <ashley@baldock.me>
2018-12-18 14:36:10 +0000

    Add failing test to force us to add Scan method

--- CheckoutKata.Tests/Checkout.Tests.cs
+++ CheckoutKata.Tests/Checkout.Tests.cs
@@ -12,5 +12,18 @@ namespace CheckoutKata.Tests
 
             Assert.Equal(0, checkout.GetTotalPrice());
         }
+
+        [Fact]
+        public void GivenACheckoutWithSomeItemsAndNoPricingRules_GetTotalPrice_ReturnsZero()
+        {
+            var checkout = new Checkout();
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            checkout.Scan("D");
+
+            Assert.Equal(0, checkout.GetTotalPrice());
+        }
     }
 }
